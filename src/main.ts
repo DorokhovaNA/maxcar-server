74c078c6c9dd9b102e6704f9db5c3ab9c864c2b7
DorokhovaNA <nisan261lib@mail.ru>
2022-10-09 01:47:41 +0300

    Revert "fix: cors"
    This reverts commit 3c750be050876933285dafe716c7d1a0c17071b2.

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(process.cwd(), 'public'));
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
