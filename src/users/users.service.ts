e55bd676507926b729b31c9c2798088359a9593f
DorokhovaNA <nisan261lib@mail.ru>
2023-01-04 22:04:50 +0300

    fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UsersDocument } from 'src/schemas/users.schema';
 import { LoginUserDto } from 'src/auth/dto/login-user.dto';
-import { hash } from 'src/utils/crypto';
+import { hash } from '../utils/crypto';
 
 @Injectable()
 export class UsersService {
